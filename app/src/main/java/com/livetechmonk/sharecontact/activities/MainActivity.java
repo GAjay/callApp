8a716f76121c9d3efd8a103f7d7691d10fe5cdc2
ajay.maheshwari@ranosys.com <gdjkjhome>
2017-06-09 15:15:11 +0530

    My first commit for project setup.

---
+++ app/src/main/java/com/livetechmonk/sharecontact/activities/MainActivity.java
@@ -0,0 +1,19 @@
+package com.livetechmonk.sharecontact.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.livetechmonk.sharecontact.R;
+
+/**
+ * @author : Ajay Kumar Maheshwari
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
